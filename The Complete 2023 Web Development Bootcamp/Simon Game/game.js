cefee5615f8512ec5289d3df730662b3059f0b40
Randy Huynh <huynhr@yorku.ca>
2023-04-25 15:03:27 -0400

    updated

--- The Complete 2023 Web Development Bootcamp/Simon Game/game.js
+++ The Complete 2023 Web Development Bootcamp/Simon Game/game.js
@@ -1,18 +1,35 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
+var currentLevel = 0;
 
 function nextSequence(gamePattern){
+    userClickedPattern = []
+    level += 1;
+    $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random() * buttonColours.length);
     var randomChosenColour  = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-    $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+    animateButton(randomChosenColour);
     playSound(randomChosenColour);
 }
 
 function playSound(colour){
     var audio = new Audio('sounds/'+colour+".mp3");
     audio.play(); 
+}
+
+function checkAnswer(currentLevel){
+    // alert(gamePattern[currentLevel] + "," + userClickedPattern[currentLevel]);
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        return true;
+    }
+    return false;
+}
+
+function animateButton(colour){
+    $("#" + colour).fadeOut(100).fadeIn(100);
 }
 
 $(document).on('keypress', function(){
@@ -22,5 +39,24 @@ $(document).on('keypress', function(){
 $(".btn").on("click", function() {
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
-    playSound(userChosenColour);
+    currentLevel = userClickedPattern.length - 1;
+    if (checkAnswer(currentLevel)){
+        animateButton(userChosenColour);
+        playSound(userChosenColour);
+        if (currentLevel === level-1){
+            setTimeout(function(){
+                nextSequence(gamePattern)
+            }, 1000);
+        }
+    }
+    else {
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        $("body").toggleClass("game-over");
+        setTimeout(function(){
+            $("body").toggleClass("game-over");
+        }, 200);
+        level = 0;
+        gamePattern = [];
+    }
 });
+
