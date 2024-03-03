326e61c39d965d3060b6a7c3d2306b0733445dca
artmedia1 <Randy_Huynh@hotmail.com>
2024-03-02 23:32:48 -0500

    Updated Sample code

--- Spring Boot and Spring Framework/Getting Started With Java Spring Framework/learning-springboot-part-02/src/main/java/com/randy/learningspringboot/SimpleExample/AppGamingSpringBean.java
+++ Spring Boot and Spring Framework/Getting Started With Java Spring Framework/learning-springboot-part-02/src/main/java/com/randy/learningspringboot/SimpleExample/AppGamingSpringBean.java
@@ -4,18 +4,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
-
 @Configuration
 @ComponentScan
 public class AppGamingSpringBean {
 
     public static void main(String[] args) {
         try (var context = new AnnotationConfigApplicationContext(AppGamingSpringBean.class);) {
-            context.getBean(GamingConsole.class).up();
-            context.getBean(GameRunner.class).run();
-
-            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
         }
     }
 }
