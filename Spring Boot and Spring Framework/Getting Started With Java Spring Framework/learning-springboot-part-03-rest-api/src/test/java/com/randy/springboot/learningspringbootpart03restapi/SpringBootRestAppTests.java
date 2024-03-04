c369c95656a377d509369ed24e5800f5131e651a
artmedia1 <Randy_Huynh@hotmail.com>
2024-03-04 11:30:29 -0500

    init

---
+++ Spring Boot and Spring Framework/Getting Started With Java Spring Framework/learning-springboot-part-03-rest-api/src/test/java/com/randy/springboot/learningspringbootpart03restapi/SpringBootRestAppTests.java
@@ -0,0 +1,13 @@
+package com.randy.springboot.learningspringbootpart03restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
